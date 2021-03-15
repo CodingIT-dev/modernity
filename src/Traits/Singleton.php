1b41083f12f37929c0cc065561ed6550e82524da
Aaron Holbrook <aaron@zeek.com>
2021-03-15 09:53:21 -0500

    Add pattern class for singleton

--- src/Traits/Singleton.php
+++ src/Traits/Singleton.php
@@ -2,9 +2,20 @@
 
 namespace Zeek\Modernity\Traits;
 
+/**
+ * Trait Singleton
+ * @package    Zeek\Modernity\Traits
+ *
+ * @deprecated Use the Singleton Class instead
+ */
 trait Singleton {
 	protected static $instance;
 
+	/**
+	 * @return mixed
+	 *
+	 * @deprecated Use the Singleton Class instead
+	 */
 	public static function instance() {
 		if ( empty( self::$instance ) ) {
 			$class          = get_called_class();
